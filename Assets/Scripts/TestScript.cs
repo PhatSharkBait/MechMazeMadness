a42b2dd61df24ca8b022ce54c9a96ba6990565d1
Cameron Bass <Cameronjudd98@gmail.com>
2022-01-09 16:50:29 -0700

    Testing Branches

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hi, I'm on the camera");
+    }
+    
+}
